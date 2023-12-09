85fca6c48f76fbed653b59c17cd968df3208ff3b
namickey <teethdai@gmail.com>
2023-12-10 08:24:24 +0900

    add 照会画面

--- src/main/java/com/example/demo/web/mapper/ItemMapper.java
+++ src/main/java/com/example/demo/web/mapper/ItemMapper.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.entity.Item;
-//import com.example.demo.web.ba02.ItemSearchCriteria;
+import com.example.demo.web.ba02.ItemSearchCriteria;
 
 /**
  * ItemMapper
@@ -20,19 +20,19 @@ public interface ItemMapper {
      */
     int insertItem(Item item);
 
-    // /**
-    //  * 全件検索
-    //  * @param criteria 検索条件
-    //  * @return itemリスト
-    //  */
-    // List<Item> findAll(ItemSearchCriteria criteria);
+    /**
+     * 全件検索
+     * @param criteria 検索条件
+     * @return itemリスト
+     */
+    List<Item> findAll(ItemSearchCriteria criteria);
 
-    // /**
-    //  * 全件カウント
-    //  * @param criteria 検索条件
-    //  * @return 件数
-    //  */
-    // long countAll(ItemSearchCriteria criteria);
+    /**
+     * 全件カウント
+     * @param criteria 検索条件
+     * @return 件数
+     */
+    long countAll(ItemSearchCriteria criteria);
 
     /**
      * 全件検索item名称指定
