c011da8e8cfcd29565e504b322d8e528e53efa10
namickey <teethdai@gmail.com>
2023-12-09 23:48:58 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spring-boot3-train
+
+Spring Security 5.4〜6.0でセキュリティ設定の書き方が大幅に変わる件
+https://qiita.com/suke_masa/items/908805dd45df08ba28d8
+
---
+++ src/main/java/com/example/demo/core/security/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * SpringSecurity6.2設定
+ */
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        // ログイン
+        .formLogin(login -> login
+            .defaultSuccessUrl("/", true)
+            .permitAll()
+        // ログアウト
+        ).logout(logout -> logout
+            .logoutSuccessUrl("/login")
+        ).authorizeHttpRequests(authz -> authz
+            // その他は認証する
+            .anyRequest().authenticated()
+        );
+        return http.build();
+
+        // 参考：以下、spring security5.7の設定
+        // http
+        //         .authorizeRequests()
+        //         // 開発向け、h2DBコンソールは認証しない
+        //         .mvcMatchers("/h2-console/**").permitAll()
+        //         // ヘルスチェックURLは認証しない
+        //         .mvcMatchers("/actuator/health").permitAll()
+        //         // item更新画面とitem削除画面はDATA_MANAGERロールを保持しているユーザのみアクセス可能
+        //         .mvcMatchers("/WBA0401/**", "/WBA0501/**").hasRole("DATA_MANAGER")
+        //         // その他は認証する
+        //         .anyRequest().authenticated()
+        //         // ログイン
+        //         .and().formLogin().defaultSuccessUrl("/", true)
+        //         // ログアウト
+        //         .and().logout((logout) -> logout.permitAll())
+        //         // 開発向け、h2DBコンソール向け設定
+        //         .csrf().ignoringAntMatchers("/h2-console/**")
+        //         .and().headers().frameOptions().sameOrigin();
+        // return http.build();
+    }
+}
